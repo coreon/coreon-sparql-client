1ed6c8ec84da70a06b5ac01bf24a74c62e86a4e1
frexos <e.georgantzos@gmail.com>
2021-08-17 16:13:34 +0300

    Remove testing logs

--- html/assets/elg/common.js
+++ html/assets/elg/common.js
@@ -45,10 +45,7 @@ define("elg/common", ["jquery", "mdc"], function ($, mdc) {
         ElgCommon.prototype.renderRepoMeta = function (metaFile) {
             var parser = new DOMParser();
             var newDoc = parser.parseFromString(metaFile, "text/html");
-            console.log('renderRepoMeta says: starting')
-            console.log('renderRepoMeta says: metaFile', metaFile)
             var this_ = this;
-            // var samplesDoc = $($.parseHTML(metaFile));
             var samplesDoc = $(newDoc);
             samplesDoc.find(".coreon-sample-query").each(function(i, elt) {
                 var s = $(elt);
@@ -58,9 +55,6 @@ define("elg/common", ["jquery", "mdc"], function ($, mdc) {
                     'htmlClass': 'js-sample_'+i
                 })
             });
-
-            console.log('renderRepoMeta says: samplesDoc', samplesDoc)
-            console.log('renderRepoMeta says: queries', samplesDoc.find(".coreon-sample-query"))
             if (this_.samples.length > 0) {
                 $(".js-samples").removeClass("hidden");
                 this_.samples.each(function(i, elt) {
@@ -86,7 +80,6 @@ define("elg/common", ["jquery", "mdc"], function ($, mdc) {
                                 this_.endpointUrl = distro.access_location;
                                 this_.samplesFile = distro.samples_location[0];
                                 $.ajax({ url: this_.samplesFile, success: function(data) {
-                                    console.log(data);
                                     this_.renderRepoMeta(data);
                                 } });
 
